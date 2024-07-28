89c49725167d734223b9d35064438732f57858f1
matheus martins <mathews_2010@outlook.com>
2024-07-28 19:08:54 -0300

    feat: connection with apollo server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const TrackAPI = require('./datasources/track-api');
+const GithubAPI = require('./datasources/github-api');
+const RickAndMortyAPI = require('./datasources/rick-and-morty-api');
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+          githubAPI: new GithubAPI({ cache }),
+          rickAndMortyAPI: new RickAndMortyAPI({ cache }),
+        },
+      };
+    },
+  });
+
+  console.log(`
+    🚀  Server is running
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
